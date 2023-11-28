9944d8ed1e864f99384df6d766b9e822efc6cabe
minhphamhk123 <minhphamhklovetory@gmail.com>
2023-11-28 10:35:06 +0700

    New UI

--- furniture_shop_admin/app/api/auth/[...nextauth]/route.ts
+++ furniture_shop_admin/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,11 @@
-import NextAuth from "next-auth"
+import NextAuth, {getServerSession} from "next-auth"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "../../../../lib/mongodb"
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+const adminEmails = ['20521003@gm.uit.edu.vn'];
+
+export const authOptions = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -11,6 +13,24 @@ const handler = NextAuth({
     })
   ],
   adapter: MongoDBAdapter(clientPromise),//goi toi file mongodb, neu da co connection thi dung khong thi tao moi
+  callbacks: {
+    session: ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
 })
 
-export { handler as GET, handler as POST }
+export {authOptions as GET, authOptions as POST};
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
--- furniture_shop_admin/components/Nav.js
+++ furniture_shop_admin/components/Nav.js
@@ -2,24 +2,29 @@
 
 import Link from "next/link";
 import { usePathname, useRouter } from 'next/navigation'
+import { signOut } from "next-auth/react";
+import Logo from "./Logo";
 
-export default function Home() {
+
+export default function Nav({show}) {
   const inActiveLink = "flex gap-1 p-1";
   const activeLink = inActiveLink + " bg-white text-blue-900 rounded-l-lg";
+  const inactiveIcon = 'w-6 h-6';
+  const activeIcon = inactiveIcon + ' text-primary';
 
   const router = useRouter();
-  console.log(router);
   const pathName = usePathname();
+
+  async function logout() {
+    await router.push('/');
+    await signOut();
+  }
+
   return (
-    <aside className="p-4 pr-0">
-      <Link href={'/'} className="flex gap-1 mb-4 mr-4">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
-        </svg>
-        <span className="">
-          FurnitureAdmin
-        </span>
-      </Link>
+    <aside className={(show?'left-0':'-left-full')+" top-0 text-gray-500 p-4 pr-0 fixed w-full bg-bgGray h-full md:static md:w-auto transition-all"}>
+      <div className="mb-4 mr-4">
+        <Logo />
+      </div>
       <nav className="flex flex-col gap-1">
         <Link href={'/'} className={pathName === '/' ? activeLink : inActiveLink}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -32,6 +37,12 @@ export default function Home() {
             <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
           </svg>
           Products
+        </Link>
+        <Link href={'/categories'} className={pathName.includes('/categories') ? activeLink : inActiveLink}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={pathName.includes('/categories') ? activeIcon : inactiveIcon}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+          </svg>
+          Categories
         </Link>
         <Link href={'/orders'} className={pathName.includes('/orders') ? activeLink : inActiveLink}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -46,6 +57,12 @@ export default function Home() {
           </svg>
           Settings
         </Link>
+        <button onClick={logout} className={inActiveLink}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+          </svg>
+          Logout
+        </button>
       </nav>
     </aside>
   );
